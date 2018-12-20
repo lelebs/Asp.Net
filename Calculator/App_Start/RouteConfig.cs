d45cace93ab8c9a3eb700d974621e7cd0f9d23a6
lelebs <lelebs@users.noreply.github.com>
2018-12-20 19:01:00 -0200

    Alterado estilo, route e criado a classe pessoa

--- Calculator/App_Start/RouteConfig.cs
+++ Calculator/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Calculator
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Calculator/Models/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class Pessoa
+    {
+        private int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [StringLength(50)]
+        private string Nome { get; set; }
+        private DateTime DataCadastro { get; set; }
+        private DateTime DataAlterecao { get; set; }
+        private string CpfCnpj { get; set; }
+        private DateTime DataNascimento { get; set; }
+    }
+}
